723a2ac71e22c12bd6efa363bdd581299b8bd89a
Josh Clancy <joshclan_-c@outlook.com>
2025-01-30 14:34:58 -0700

    74 percent coverage

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,8 +1,24 @@
 const request = require('supertest');
 const app = require('../service');
+const e = require('express');
+const { Role, DB } = require('../database/database.js');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+let userId;
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  const auth = await app.setAuthUser(user);
+  return { ...user, password: 'toomanysecrets', token: auth };
+}
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
@@ -20,6 +36,29 @@ test('login', async () => {
   delete expectedUser.password;
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
+
+test('updateUser Not Admin', async () => {
+  const updatedUserData = {
+    name: 'Updated Test User',
+    email: 'updated_' + testUser.email,
+    password: 'a'
+  };
+
+  const updateRes = await request(app)
+    .put('/api/auth/' + testUser.id)
+    .set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send(updatedUserData);
+    expect(updateRes.status).toBe(403);
+    expect(updateRes.body.message).toBe('unauthorized');
+  });
+
+test('logout', async () => {
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+});
+
+
 
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,119 @@
+const request = require('supertest');
+const app = require('../service');
+const e = require('express');
+const { Role, DB } = require('../database/database.js');
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+let adminAuthToken;
+
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+  }
+
+
+  
+
+
+  beforeAll(async () => {
+    if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+    jest.setTimeout(60 * 1000 * 5); // 5 minutes
+  }
+  });
+
+
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
+
+
+
+test('not admin create franchise', async () => {
+    testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    expectValidJwt(testUserAuthToken);
+
+
+    const res = await request(app)
+        .post('/api/franchise')
+        .send({ name: 'franchise' })
+        .set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(res.status).toBe(403);
+    expect(res.body.message).toBe('unable to create a franchise');
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+});
+
+test('admin create franchise', async () => {
+    const admin = await createAdminUser();
+    const loginRes = await request(app).put('/api/auth').send(admin);
+    adminAuthToken = loginRes.body.token;
+    expectValidJwt(adminAuthToken);
+
+    const franchiseRequest = {
+      name: admin.name + ' franchise',
+      admins: [{ email: admin.email }], // Only sending email as expected by createFranchise
+    }
+
+  const res = await request(app)
+  .post('/api/franchise')
+  .set('Authorization', `Bearer ${adminAuthToken}`)
+  .send(franchiseRequest);
+
+  expect(res.status).toBe(200);
+  });
+
+test('get franchises', async () => {
+    const getFranchiseRes = await request(app).get('/api/franchise');
+    expect(getFranchiseRes.status).toBe(200);
+    expect(getFranchiseRes.body.length).toBeGreaterThan(0);
+});
+
+test ('get user franchises', async () => {
+    const getFranchiseRes = await request(app).get('/api/franchise');
+    const franchiseId = getFranchiseRes.body[0].id;
+    const getUserFranchiseRes = await request(app).get(`/api/franchise/${franchiseId}`).set('Authorization', `Bearer ${adminAuthToken}`);
+    expect(getUserFranchiseRes.status).toBe(200);
+});
+
+test( 'create store', async () => {
+    const getFranchiseRes = await request(app).get('/api/franchise');
+    const franchiseId = getFranchiseRes.body[0].id;
+    const createStoreRes = await request(app)
+    .post(`/api/franchise/${franchiseId}/store`)
+    .set('Authorization', `Bearer ${adminAuthToken}`)
+    .send({ name: 'store' });
+    expect(createStoreRes.status).toBe(200);
+});
+
+test('delete store admin', async () => {
+    const getFranchiseRes = await request(app).get('/api/franchise');
+    const franchiseId = getFranchiseRes.body[0].id;
+    const storeId = getFranchiseRes.body[0].stores[0].id;
+    const deleteRes = (await request(app)
+    .delete(`/api/franchise/${franchiseId}/store/${storeId}`)
+    .set('Authorization', `Bearer ${adminAuthToken}`));
+    expect(deleteRes.status).toBe(200);
+    expect(deleteRes.body.message).toBe('store deleted');
+});
+
+test('delete franchise', async () => {
+    const getFranchiseRes = await request(app).get('/api/franchise');
+    const franchiseId = getFranchiseRes.body[0].id;
+    const deleteRes = (await request(app)
+    .delete(`/api/franchise/${franchiseId}`)
+    .set('Authorization', `Bearer ${adminAuthToken}`));
+    expect(deleteRes.status).toBe(200);
+});
+
+
+function expectValidJwt(potentialJwt) {
+    expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  }
